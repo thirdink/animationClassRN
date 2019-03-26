f587a4af39f63fda28896e9c01d1695d5b5d9cbe
Rumit Tuladhar <rumitwashere@gmail.com>
2019-03-26 11:16:28 +0545

    opacity

---
+++ App.js
@@ -0,0 +1,59 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {
+  Platform, StyleSheet, Text, View, Animated, TouchableWithoutFeedback
+} from 'react-native';
+
+
+
+type Props = {};
+export default class App extends Component<Props> {
+  state= {
+    animation: new Animated.Value(0) // default opacity to 1 otherwise it will be hidden
+  }
+  startAnimation = () =>{
+    Animated.timing(this.state.animation,{
+      toValue: 0,
+      duration: 350
+    }).start(()=>{
+      Animated.timing(this.state.animation,{
+        toValue:1,
+        duration: 500
+      }).start()
+    });
+  }
+
+  render() {
+    const animatedStyles = {
+      opacity: this.state.animation
+    }
+    return (
+      <View style={styles.container}>
+       <TouchableWithoutFeedback onPress={this.startAnimation}>
+        <Animated.View style={[styles.box,animatedStyles]}/>
+       </TouchableWithoutFeedback>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  box: {
+    width: 150,
+    height: 150,
+    backgroundColor: 'tomato',
+  }
+});
